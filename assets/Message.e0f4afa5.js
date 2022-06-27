f7142f7e5e80f7e2ec2e60d28a766c5c6048a6df
RibsLi <2234692411@qq.com>
2022-06-27 22:31:23 +0800

    deploy

--- assets/Message.e0f4afa5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.5378c7f1.js";import{o as _,c as t}from"./vendor.62775ade.js";const s={id:"gitalk"},a={setup(o){return(r,c)=>(_(),t("div",s))}};var d=e(a,[["__scopeId","data-v-0f5317f0"]]);export{d as default};
