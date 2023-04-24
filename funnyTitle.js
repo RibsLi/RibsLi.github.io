5f189e7a62aa8e72d27f6559e832232bbda2be56
RibsLi <2234692411@qq.com>
2023-04-25 00:54:32 +0800

    deploy

--- funnyTitle.js
+++ funnyTitle.js
@@ -1,24 +1,15 @@
 // 浏览器搞笑标题
-// let OriginTitle = document.title
 let titleTime
 let link = document.querySelector('link[rel*="icon"]') || document.createElement('link')
-link.rel = 'icon'
-if (link.href.indexOf('unknown')) {
-  link.href = '/favicon.ico'
-}
+
 document.addEventListener('visibilitychange', function () {
+  link.href = '/favicon.ico'
   if (document.hidden) {
-    // link.href = "/failure.ico"
-    // document.title = "(ฅ>ω<*ฅ)藏好啦 ~"
-    link.href = '/favicon.ico'
     document.title = '一个靓仔失去了梦想 ฅ ̳͒•ˑ̫• ̳͒ฅ'
     clearTimeout(titleTime)
   } else {
-    link.href = '/favicon.ico'
-    // document.title = "ฅ ̳͒•ˑ̫• ̳͒ฅ被发现啦 ~" + OriginTitle
     document.title = '一个有梦想的靓仔 (ฅ>ω<*ฅ)'
     titleTime = setTimeout(function () {
-      // document.title = OriginTitle
       document.title = localStorage.getItem('docTitle')
     }, 1500)
   }
